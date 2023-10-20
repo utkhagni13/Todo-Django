e91c7a84e998f5cf417661fbc290aa497da1d2f5
Utkarsh Agnihotri <utkhagni13@gmail.com>
2023-10-20 11:49:16 +0530

    create apis

--- rest/todo/urls.py
+++ rest/todo/urls.py
@@ -1,11 +1,11 @@
 # todo_list/todo_app/urls.py
 from django.urls import path
-from .views import TodoListView
+from .views import TodoListView, TodoListAdd
 
 urlpatterns = [
     # URL for fetching the list
-    path("todo/", TodoListView.as_view(), name="todo-list"),
+    path("", TodoListView.as_view(), name="todo-list"),
 
     # URL for fetching the list
-    path("todo/addtodo/", TodoListView.as_view(), name="add-task"),
+    path("addtodo/", TodoListAdd.as_view(), name="add-task"),
 ]
--- rest/todo/views.py
+++ rest/todo/views.py
@@ -3,19 +3,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from pymongo import MongoClient
-from bson import ObjectId
+import os
 
-mongo_uri = "mongodb+srv://agnibit1309:yo1RyuTHEdQ51Nyx@todo-django.xykmx69.mongodb.net/?retryWrites=true&w=majority"
-db = MongoClient(mongo_uri)['test_db']
-collection = db['todo1']
+mongo_uri = os.getenv("DATABASE_URL")
+client = MongoClient(mongo_uri)
+db = client[os.getenv("DATABASE_NAME")]
+collection = db[os.getenv("COLLECTION_NAME")]
 
 
 class TodoListView(APIView):
     def get(self, request):
         # Implement this method - return all todo items from db instance above.
-        print("fetch: ", request)
         try:
-            todos = list(collection.find({}))
+            data = collection.find()
+            todos = [
+                {
+                    '_id': str(obj.get('_id')),  # Convert ObjectId to string
+                    'title': obj.get('title'),
+                }
+                for obj in data
+            ]
             if (todos.__len__):
                 return Response({"data": todos, "error": None}, status=status.HTTP_200_OK)
             else:
@@ -23,19 +30,38 @@ class TodoListView(APIView):
         except:
             return Response({"data": None, "error": Exception}, status=status.HTTP_400_BAD_REQUEST)
 
+
+class TodoListAdd(APIView):
     def post(self, request):
         # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        print("add: ", request)
-        title = request.title
+        title = request.data["title"]
         try:
-            existing_task = collection.find_one({"task": title})
+            existing_task = collection.find_one({'title': title})
+            # print()
+            # print("existing: ", existing_task)
+            # print()
             if existing_task:
                 return Response({"data": None, "error": "Already Exists"}, status=status.HTTP_409_CONFLICT)
-
-            todo_data = {}
-            todo_data["id"] = ObjectId()
-            todo_data["title"] = title
-            result = collection.insert_one(todo_data)
-            return Response({"data": result.id, "error": None}, status=status.HTTP_200_OK)
+            print()
+            print("existing:", existing_task)
+            print()
+            data = collection.insert_one({'title': title})
+            print()
+            print("data:", data)
+            print()
+            # result = [
+            #     {
+            #         '_id': str(obj.get('_id')),  # Convert ObjectId to string
+            #         'title': obj.get('title'),
+            #     }
+            #     for obj in data
+            # ]
+            # print()
+            # print("res:", result)
+            # print()
+            if data:
+                return Response({"data": "Success", "error": None}, status=status.HTTP_200_OK)
+            else:
+                return Response({"data": None, "error": "Database Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         except:
             return Response({"data": None, "error": Exception}, status=status.HTTP_400_BAD_REQUEST)
